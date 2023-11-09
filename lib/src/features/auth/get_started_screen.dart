95d4060984f5379b730844729677cb37eb055335
Jan Ndungu <ndungujan23@protonmail.com>
2023-11-09 15:36:55 +0300

    updated get started screen

--- lib/src/features/auth/get_started_screen.dart
+++ lib/src/features/auth/get_started_screen.dart
@@ -71,44 +71,46 @@ class GetStartedScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SafeArea(
-        child: Scaffold(
-            bottomNavigationBar: _footerContent(context),
-            body: Center(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 13.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/images/logo.png',
-                        height: 100,
-                      ),
-                      const SizedBox(height: 30),
-                      Text(
-                        'Let\'s Get Started',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                            fontSize: 26,
-                            fontWeight: FontWeight.w700,
-                            color: Theme.of(context).colorScheme.onBackground),
-                      ),
-                      const SizedBox(height: 15),
-                      Text(
-                        'With Swift Pay, sending and receiving money has never been easier',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                            fontSize: 14,
-                            color: Theme.of(context).colorScheme.secondary),
-                      ),
-                    ],
-                  ),
+    return Scaffold(
+        appBar: AppBar(),
+        bottomNavigationBar: _footerContent(context),
+        body: SafeArea(
+          child: Center(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 13.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/logo.png',
+                      height: 100,
+                    ),
+                    const SizedBox(height: 30),
+                    Text(
+                      'Let\'s Get Started',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
+                          color: Theme.of(context).colorScheme.onBackground),
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      'With Swift Pay, sending and receiving money has never been easier',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          fontSize: 14,
+                          color: Theme.of(context).colorScheme.secondary),
+                    ),
+                  ],
                 ),
               ),
-            )));
+            ),
+          ),
+        ));
   }
 }
--- lib/src/features/auth/sign_in_screen.dart
+++ lib/src/features/auth/sign_in_screen.dart
@@ -4,10 +4,47 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class SignInScreen extends ConsumerWidget {
   const SignInScreen({super.key});
 
+  void onSignIn() {
+    print("Sign in");
+  }
+
+  Widget _footerSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.all(
+            12.0,
+          ),
+          child: Expanded(
+              child: SizedBox(
+            height: 50,
+            width: MediaQuery.of(context).size.width,
+            child: FilledButton(
+              onPressed: onSignIn,
+              style: FilledButton.styleFrom(
+                  backgroundColor:
+                      Theme.of(context).colorScheme.primaryContainer),
+              child: Text(
+                'Sign In',
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimaryContainer),
+              ),
+            ),
+          )),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(),
+      bottomNavigationBar: _footerSection(context),
       body: SafeArea(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -40,7 +77,29 @@ class SignInScreen extends ConsumerWidget {
                   ),
                 ),
                 const SizedBox(
-                  height: 40,
+                  height: 45,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    suffixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    suffixIcon: Icon(Icons.visibility_off),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
                 ),
               ],
             ),
