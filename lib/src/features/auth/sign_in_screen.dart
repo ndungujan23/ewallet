74167c024b898568c20db7c0b7b1b409af9df171
Jan Ndungu <ndungujan23@protonmail.com>
2023-11-09 14:59:23 +0300

    started working on sign in screen

--- lib/src/features/auth/sign_in_screen.dart
+++ lib/src/features/auth/sign_in_screen.dart
@@ -7,12 +7,46 @@ class SignInScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome'),
-      ),
-      body: const SafeArea(
-        child: Text('body'),
-      ),
+      appBar: AppBar(),
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 10),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'Welcome back 👋',
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontSize: 26,
+                        ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'Please enter your email and password to sign in',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
