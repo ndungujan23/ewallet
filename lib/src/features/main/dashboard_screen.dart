5392f5be2ce97b27b86493c5d85119c68c0c325a
Jan Ndungu <ndungujan23@protonmail.com>
2023-11-10 19:37:41 +0300

    updated dashboard screen

--- lib/src/features/main/dashboard_screen.dart
+++ lib/src/features/main/dashboard_screen.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
 
+  String greeting() {
+    var hour = DateTime.now().hour;
+    return hour < 12 ? 'Morning' : hour < 17 ? 'Afternoon' : 'Morning';
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text('Welcome'),
+        // backgroundColor: Color(0x44000000),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text("Title"),
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
       ),
-      body: const SafeArea(
-        child: Text('body'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: const Center(child: Text("Content")),
+          ),
+        ),
       ),
     );
   }
