41cf00ac5d954f5c6fc04fa768a0e881811b602d
Jan Ndungu <ndungujan23@protonmail.com>
2023-11-13 00:28:04 +0300

    updated dashboard layout

--- lib/src/features/main/dashboard_screen.dart
+++ lib/src/features/main/dashboard_screen.dart
@@ -1,6 +1,6 @@
+import 'package:ebank/src/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ebank/src/ui/widgets/index.dart';
 
 class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
@@ -10,26 +10,48 @@ class DashboardScreen extends ConsumerWidget {
     var timeOfDay = hour <= 12
         ? 'Morning'
         : hour <= 17
-        ? 'Afternoon'
-        : 'Evening';
+            ? 'Afternoon'
+            : 'Evening';
     return "Good $timeOfDay 👋";
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: const TopBar(),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
+        child: Container(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: const Center(child: Text("Content")),
-          ),
-        ),
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 24.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        greeting(),
+                        textAlign: TextAlign.left,
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                      ),
+                      Text(
+                        'Amanda Alex',
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                            ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
--- lib/src/ui/widgets/topbar.dart
+++ lib/src/ui/widgets/topbar.dart
@@ -1,67 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   final Size preferredSize;
 
-  const TopBar({super.key})
-      : preferredSize = const Size.fromHeight(kToolbarHeight);
+  const TopBar({super.key}) : preferredSize = const Size.fromHeight(kToolbarHeight * 2);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 10.0),
+    return Container(
+      color: Theme.of(context).colorScheme.primaryContainer,
+      padding: const EdgeInsets.all(8.0),
       child: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         forceMaterialTransparency: true,
         elevation: 0.0,
-        leadingWidth: 40,
         titleSpacing: 0,
         leading: Row(
           children: [
             GestureDetector(
-              onTap: () { context.push('/profile'); },
+              onTap: () {
+                context.push('/profile');
+              },
               child: const CircleAvatar(
                 backgroundImage: AssetImage('assets/images/avatar.png'),
               ),
             ),
           ],
         ),
-        title: Container(
-          margin: const EdgeInsets.only(left: 12.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: Colors.grey[200]
-            // color: Theme.of(context).colorScheme.surface,
-          ),
-          child: const TextField(
-            readOnly: true,
-            decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.search,
-                color: Colors.grey,
+        centerTitle: true,
+        title: Text(
+          'SwiftPay',
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
               ),
-              border: InputBorder.none,
-              hintStyle: TextStyle(color: Colors.grey),
-              hintText: "Search",
-            ),
-          ),
         ),
         actions: [
+          GestureDetector(onTap: () {}, child: const Icon(Icons.search)),
           IconButton(
             onPressed: () {},
             icon: const Badge(
               label: Text('10'),
               child: Icon(
-                Icons.notifications,
+                Icons.notifications_outlined,
                 size: 26,
               ),
             ),
           ),
         ],
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(150),
+          child: SizedBox(
+            height: 35,
+            child: TextFormField(
+              textAlign: TextAlign.start,
+              // controller: _searchText,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: InkWell(
+                  child: const Icon(Icons.close),
+                  onTap: () {
+                    // _searchText.clear();
+                  },
+                ),
+                hintText: 'Search...',
+                contentPadding: const EdgeInsets.all(0),
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
