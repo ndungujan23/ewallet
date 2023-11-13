c4115f0a3524f235e9154466b593c94fa6700b55
Jan Ndungu <ndungujan23@protonmail.com>
2023-11-13 05:26:48 +0300

    updated dashboard page

--- lib/src/features/main/dashboard_screen.dart
+++ lib/src/features/main/dashboard_screen.dart
@@ -1,8 +1,9 @@
-import 'package:card_swiper/card_swiper.dart';
-import 'package:ebank/src/core/index.dart';
-import 'package:ebank/src/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:card_swiper/card_swiper.dart';
+
+import 'package:ebank/src/ui/widgets/index.dart';
+import 'package:ebank/src/core/index.dart';
 
 class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
@@ -23,21 +24,18 @@ class DashboardScreen extends ConsumerWidget {
       extendBodyBehindAppBar: true,
       appBar: const TopBar(),
       body: SafeArea(
-        child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.only(left: 16, right: 16, top: 24.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // _greetingSection(context, greeting()),
-                  // const SizedBox(height: 15,),
-                  _walletListSection(context, greeting()),
-                ],
-              ),
-            )),
-      ),
+          child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+                padding: const EdgeInsets.only(left: 16, right: 16, top: 24.0),
+                child: _walletListSection(context, greeting())),
+            // _insightSection(context),
+          ],
+        ),
+      )),
     );
   }
 }
@@ -46,6 +44,8 @@ Widget _walletListSection(BuildContext context, String greeting) {
   return Column(
     children: [
       AppBar(
+        backgroundColor: Colors.transparent,
+        forceMaterialTransparency: true,
         titleSpacing: 0,
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -56,6 +56,7 @@ Widget _walletListSection(BuildContext context, String greeting) {
               style: Theme.of(context).textTheme.labelLarge?.copyWith(
                     fontSize: 16,
                     fontWeight: FontWeight.w400,
+                    color: Theme.of(context).colorScheme.onBackground,
                   ),
             ),
             Text(
@@ -63,6 +64,7 @@ Widget _walletListSection(BuildContext context, String greeting) {
               style: Theme.of(context).textTheme.labelLarge?.copyWith(
                     fontSize: 22,
                     fontWeight: FontWeight.w700,
+                    color: Theme.of(context).colorScheme.onBackground,
                   ),
             )
           ],
@@ -76,22 +78,26 @@ Widget _walletListSection(BuildContext context, String greeting) {
       const SizedBox(
         height: 15.0,
       ),
-      Swiper(
-        itemCount: 5,
-        layout: SwiperLayout.TINDER,
-        itemWidth: MediaQuery.of(context).size.width,
-        itemHeight: 340,
-        itemBuilder: (c, i) {
-          return WalletAccountCard(
-              account: WalletAccount(
-            id: i + 1,
-            accountNumber: '0950172588513',
-            amount: 200000,
-            type: WalletAccountType.savings,
-            validityPeriod: DateTime.now(),
-          ));
-        },
-      )
+      // Swiper(
+      //   itemCount: 5,
+      //   layout: SwiperLayout.TINDER,
+      //   itemWidth: MediaQuery.of(context).size.width,
+      //   itemHeight: 340,
+      //   itemBuilder: (c, i) {
+      //     return WalletAccountCard(
+      //         account: WalletAccount(
+      //       id: i + 1,
+      //       accountNumber: '0950172588513',
+      //       amount: 200000,
+      //       type: WalletAccountType.savings,
+      //       validityPeriod: DateTime.now(),
+      //     ));
+      //   },
+      // )
     ],
   );
+}
+
+Widget _insightSection(BuildContext context) {
+  return const Placeholder();
 }
