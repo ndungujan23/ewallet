03b145f4f5da5bae151c67e189437f51d24c5b44
Jan Ndungu <ndungujan23@protonmail.com>
2023-11-14 07:20:34 +0300

    added wallet screen

--- lib/src/features/index.dart
+++ lib/src/features/index.dart
@@ -3,3 +3,4 @@ export 'auth/get_started_screen.dart';
 export 'auth/sign_in_screen.dart';
 export 'main/dashboard_screen.dart';
 export 'main/profile/profile_screen.dart';
+export 'main/wallet/add_wallet_screen.dart';
---
+++ lib/src/features/main/wallet/add_wallet_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+
+class WalletAddScreen extends ConsumerWidget {
+  const WalletAddScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Placeholder();
+  }
+}
