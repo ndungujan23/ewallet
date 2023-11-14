6dad6bf97b2752cb752620a0d2bf40006ea69740
Jan Ndungu <ndungujan23@protonmail.com>
2023-11-14 05:23:48 +0300

    updated avatar shenanigans

---
+++ lib/src/core/data/constants/avatar.dart
@@ -0,0 +1,3 @@
+const avatarPlaceholder = [
+  'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+];
